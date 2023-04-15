f76b58e243f7c81ed259dd23e4531ffe79aff533
luan-css <sluan298@gmail.com>
2023-04-14 21:39:30 -0300

    criando IAAS com terraform

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "jenkis" {
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins"
+  region   = var.region
+  size     = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.jornada.id]
+
+}
+
+data "digitalocean_ssh_key" "jornada" {
+  name = "Jornada"
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name   = "k8s"
+  region = var.region
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.26.3-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
+
+variable "do_token" {
+  default = ""
+}
+variable "region" {
+  default = ""
+}
+output "jenkins_ip" {
+  value = digitalocean_droplet.jenkis.ipv4_address
+}
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+do_token = "dop_v1_dcf487043d4a3b7d25d3b67ae116a9abe72e85ced4c2a2aeded0e44e03ff7774"
+region   = "nyc1"
